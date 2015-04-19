c184075ae88b3b05bcce14f7c975be6758baa64f
roberts-b <robiks22@inbox.lv>
2015-04-19 17:44:40 +0300

    Finish user edit update and delete actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(name:  "robik",
+             email: "robik@robik.lv",
+             password:              "robik123",
+             password_confirmation: "robik123",
+             admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
